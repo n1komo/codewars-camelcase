3a90045f4a03c8123785c1d14951e8aec705264f
n1komo <nikomoplay@gmail.com>
2023-01-10 15:38:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def to_camel_case(text):
+    for i in range(text):
+        if i == '-' or i == '_' :
+
+    return
+
+
+
+
+sample1 = "the-stealth-warrior"
+sample2 = "The_Stealth_Warrior"
+to_camel_case(sample1)
+to_camel_case(sample2)
